b4da60c2bb3036aca0dde62f2912292f26cc3010
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-24 18:40:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday1, supplies|
+      supplies<<supply
+    end
+  end
 end
 
 
@@ -62,10 +66,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
