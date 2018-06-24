045f1a9bb67e0d55f4fa31277d0b33e81e49e211
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-24 18:55:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holiday=[]
   holiday_hash.each do |season, season_hash|
     season_hash.each do |holiday, supplies|
       supplies.each do |supply|
